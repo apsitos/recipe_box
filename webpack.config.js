40964be02e1828284037afc0fe80dcf4766c0c55
Annastasia Psitos <annastasia.psitos@gmail.com>
2017-05-10 16:50:39 -0600

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const resolve = path.resolve;
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './app',
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      { test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      { test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.png$/,
+        use: { loader: 'url-loader', options: { limit: 100000 } },
+      },
+      {
+        test: /\.jpg$/,
+        use: [ 'file-loader' ]
+      }
+    ]
+  },
+  devServer: {
+    contentBase: './build',
+    inline: true
+  },
+  resolve: {
+    extensions: ['.js', '.json', '.jsx', '.css', '.scss']
+  },
+  externals: {
+    'cheerio': 'window',
+    'react/lib/ExecutionEnvironment': true,
+    'react/lib/ReactContext': true,
+  }
+}
