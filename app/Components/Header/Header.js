d7bc009be240b83a86134a7641f2174b40ff72eb
Annastasia Psitos <annastasia.psitos@gmail.com>
2017-05-10 17:13:56 -0600

    Header renders in Main which renders on page

---
+++ app/Components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import './header-style';
+
+const Header = () => {
+  return (
+    <div className="Header">
+      <h1>Recipe Box</h1>
+    </div>
+  );
+}
+
+export default Header;
---
+++ app/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Main from './Components/Main/Main';
+
+render(<Main />, document.querySelector('.application'));
